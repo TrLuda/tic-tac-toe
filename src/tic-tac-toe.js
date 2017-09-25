099b0ad0f8e7676a8c422279b43e48a6c45c1a44
LiudmilaTrafimuk <TrLuda@tut.by>
2017-09-25 23:05:51 +0300

    Update tic-tac-toe.js

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,34 +1,72 @@
 class TicTacToe {
     constructor() {
-
+        this.matrix = [[null,null,null],
+                       [null,null,null],
+                       [null,null,null]];
+        this.winner = null;
+        this.Player = false;
+        this.symbol = 'x';
+        this.count = null;      
     }
 
-    getCurrentPlayerSymbol() {
-
+    getCurrentPlayerSymbol() {       
+        if (this.Player){
+            if (this.symbol == 'x' ? this.symbol = 'o' : this.symbol = 'x');            
+            this.Player = false;
+        }    
+        return this.symbol;       
     }
 
     nextTurn(rowIndex, columnIndex) {
+        if (this.matrix[rowIndex][columnIndex] == null){
+            this.matrix[rowIndex][columnIndex] = this.symbol; 
+            this.Player = true;
+            this.count++;             
+        }
 
+        if ((this.matrix[0][0] && this.matrix[0][0] == this.matrix[0][1] && this.matrix[0][0] == this.matrix[0][2]) ||
+            (this.matrix[1][0] && this.matrix[1][0] == this.matrix[1][1] && this.matrix[1][0] == this.matrix[1][2]) ||
+            (this.matrix[2][0] && this.matrix[2][0] == this.matrix[2][1] && this.matrix[2][0] == this.matrix[2][2]) ||
+            (this.matrix[0][0] && this.matrix[0][0] == this.matrix[1][0] && this.matrix[0][0] == this.matrix[2][0]) ||
+            (this.matrix[0][1] && this.matrix[0][1] == this.matrix[1][1] && this.matrix[0][1] == this.matrix[2][1]) ||
+            (this.matrix[0][2] && this.matrix[0][2] == this.matrix[1][2] && this.matrix[0][2] == this.matrix[2][2]) ||
+            (this.matrix[0][0] && this.matrix[0][0] == this.matrix[1][1] && this.matrix[0][0] == this.matrix[2][2]) ||
+            (this.matrix[0][2] && this.matrix[0][2] == this.matrix[1][1] && this.matrix[0][2] == this.matrix[2][0])) {
+            this.winner = this.symbol;
+        }    
+        this.getCurrentPlayerSymbol();
     }
 
     isFinished() {
-
+        if (this.getWinner() || this.isDraw()){
+            return true;
+        } else {
+            return false;
+        }      
     }
 
     getWinner() {
-
+       return this.winner;             
     }
 
     noMoreTurns() {
-
+        if (this.count == 9){
+            return true;
+        } else {
+            return false;
+        }
     }
 
     isDraw() {
-
+        if (this.noMoreTurns() && this.winner == null){
+            return true;
+        } else {
+            return false;
+        }        
     }
 
     getFieldValue(rowIndex, colIndex) {
-
+        return this.matrix[rowIndex][colIndex];               
     }
 }
 
